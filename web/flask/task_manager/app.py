e3d9b62c6393410446e1c20356c8bcf18de6439a
FayeCarter <fayecarter333@gmail.com>
2020-07-14 11:56:27 +0100

    Create delet route

--- web/flask/task_manager/app.py
+++ web/flask/task_manager/app.py
@@ -18,7 +18,7 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
   if request.method == 'POST':
-    task_content = request.frorm['content']
+    task_content = request.form['content']
     new_task = Todo(content=task_content)
 
     try:
@@ -31,6 +31,17 @@ def index():
   else:
     tasks = Todo.query.order_by(Todo.date_created).all()
     return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+  task_to_delete = Todo.query.get_or_404(id)
+
+  try:
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect('/')
+  except:
+    return 'Issue deleting task'
 
 if __name__ == "__main__":
   app.run(debug=True)
