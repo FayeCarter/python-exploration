dac9c05001bce8787851940bea18e979e546026f
FayeCarter <fayecarter333@gmail.com>
2020-07-18 11:56:23 +0100

    Add file to urls.json

--- web/flask/url-shortener/app.py
+++ web/flask/url-shortener/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'g7892o3irnewdfhs89djshgk'
@@ -21,7 +22,14 @@ def your_url():
       flash('That short name has already been taken. Please select a new name')
       return redirect(url_for('home'))
 
-    urls[request.form['code']] = {'url': request.form['url']}
+    if 'url' in request.form.keys():
+      urls[request.form['code']] = {'url': request.form['url']}
+    else:
+      new_file = request.files['file']
+      full_name = request.form['code'] + secure_filename(new_file.filename)
+      new_file.save('/Users/FayeCarter/Documents/Projects/python/web/flask/url-shortener/' + full_name)
+      urls[request.form['code']] = {'file': full_name}
+
     with open('urls.json', 'w') as url_file:
       json.dump(urls, url_file)
     return render_template('your_url.html', code=request.form['code'])
